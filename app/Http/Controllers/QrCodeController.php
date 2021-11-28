717c9c8dd80b9064fcad186d72bf6c1d1548f8c2
admin <admin@MacBook-Pro-de-Narfk.local>
2021-11-28 16:10:09 +0100

    add generate qrcode function and downloadZip function

---
+++ app/Http/Controllers/QrCodeController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Repositories\QrCodeRepository;
+use App\Http\Controllers\Controller;
+use SimpleSoftwareIO\QrCode\Facades\QrCode;
+use Illuminate\Support\Facades\Storage;
+use File;
+use ZipArchive;
+
+use Illuminate\Http\Request;
+
+class QrCodeController extends Controller
+{
+    public function generate(Request $request){
+    foreach($request->names as $name){
+        $image = \QrCode::format('png')
+                 ->size(200)
+                 ->generate($name);
+        $output_file= $name . time() . '.png';
+        Storage::put($output_file,$image); 
+        }
+    }
+    public function downloadZip()
+    {
+        $zip = new ZipArchive;
+   
+        $fileName = 'myNewFile.zip';
+   
+        if ($zip->open(public_path($fileName), ZipArchive::CREATE) === TRUE)
+        {
+            $files = File::files(public_path('storage'));
+   
+            foreach ($files as $key => $value) {
+                $relativeNameInZipFile = basename($value);
+                $zip->addFile($value, $relativeNameInZipFile);
+            }
+             
+            $zip->close();
+        }
+        Storage::delete('app\public\*');
+        return response()->download(public_path($fileName));
+    }
+
+}
